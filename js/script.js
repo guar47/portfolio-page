1ddfd116b6dc32432ef9c384d24d89255007098b
Dmitrii Pashutskii <guar47@ya.ru>
2017-12-11 22:21:15 +0300

    add color scheme

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
-  $('nav').find('a.page-link').click(function (e) {
+  $('nav').find('li.page-link').click(function (e) {
     e.preventDefault();
-    const section = $(this).attr('href');
+    const section = $(this).find('a').attr('href');
     $('html, body').animate({
       scrollTop: $(section).offset().top - $('header').outerHeight(),
     });
