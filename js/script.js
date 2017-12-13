2ebcb00868f92d86cc5c8bcea6a74ea6549608d8
Dmitrii Pashutskii <guar47@ya.ru>
2017-12-13 20:45:33 +0300

    add adaptive and animation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 $(document).ready(() => {
   $('nav').find('li.page-link').click(function (e) {
     e.preventDefault();
+
     const section = $(this).find('a').attr('href');
     $('html, body').animate({
       scrollTop: $(section).offset().top - $('header').outerHeight(),
     });
+  });
+
+  $('.toggle-nav').click(function (e) {
+    e.preventDefault();
+
+    $('nav').toggleClass('active');
+    $('main').toggleClass('active');
   });
 });
